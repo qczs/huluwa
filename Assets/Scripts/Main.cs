5c2ae3949343a0d80088b665ad00c5e37eb1ec21
mengjie <mengjie0718@163.com>
2014-03-27 17:05:29 +0800

    登录 主城

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class Main : MonoBehaviour {
-
+	private GameObject prefab;  
 	// Use this for initialization
+	public static Main main;
+	private Transform _transform;
 	void Start () {
-	
+		main = this;
+		_transform = gameObject.transform;
+		ShowView("ui/LoginView");
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void HideView(GameObject go){
+		Destroy(go);
 	}
+	public void ShowView(string path){
+		prefab = Instantiate(Resources.Load(path),_transform.position,_transform.rotation) as GameObject;
+		prefab.transform.parent = _transform;
+		prefab.transform.localScale = _transform.localScale;
+	}
+
 }
--- Assets/Scripts/ui/LoginView.cs
+++ Assets/Scripts/ui/LoginView.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class LoginView : MonoBehaviour {
-
+	public GameObject btnLogin;
 	// Use this for initialization
-	void Start () {
+	void Awake ()
+	{  //获取需要监听的按钮对象
+
+
+		//设置这个按钮的监听，指向本类的ButtonClick方法中。
+//		UIButton btn = btnLogin.GetComponent<UIButton>();
+//		btn.onClick =ButtonClick
+		UIEventListener.Get(btnLogin).onClick = ButtonClick;
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void ButtonClick(GameObject button)
+	{
+		Main.main.ShowView("ui/MainView");
+		Main.main.HideView(gameObject);	
 	}
+
+
 }
