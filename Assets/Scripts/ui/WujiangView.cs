5b00cec687458d2de0340d49a0aace601a2884df
赵鹤楠 <qczs88@gmail.com>
2014-03-29 00:34:47 +0800

    午间 Gian 版

---
+++ Assets/Scripts/ui/WujiangView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WujiangView : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ButtonClick(GameObject button)
+	{
+		Main.main.ShowView("ui/MainView");
+		Main.main.HideView(gameObject);	
+	}
+}
