6fb722da3fc68baeb19108f0420d790de30bfcc9
赵鹤楠 <qczs88@gmail.com>
2014-03-30 18:50:40 +0800

    加载XML文件

---
+++ Assets/Scripts/utils/TimeUtil.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System;
+
+public class TimeUtil
+{
+		public static long UNIX_TIMESTAMP (DateTime dateTime)
+		{
+				return (dateTime.Ticks - DateTime.Parse ("1970-01-01 00:00:00").Ticks) / 10000000;
+		}
+
+}
