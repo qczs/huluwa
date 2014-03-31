66bf548dab642d44e37e4585b7a88778a2403294
mengjie <mengjie0718@163.com>
2014-03-31 11:58:37 +0800

    自己动态加载

--- Assets/Scripts/Editor/MakeAssetBundles.cs
+++ Assets/Scripts/Editor/MakeAssetBundles.cs
@@ -14,12 +14,14 @@ static public class  MakeAssetBundles {
 		var options = BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets;
 		DirectoryInfo dir = new DirectoryInfo( Application.dataPath+"/ui");
 		FileInfo[] files = dir.GetFiles();
+
+		AssetDatabase.CreateFolder("Assets/StreamingAssets/","ui");
 		foreach(FileInfo file in files){
 			if(file.Extension==".prefab"){
 				UnityEngine.Debug.Log(file.Name);
 				BuildPipeline.BuildAssetBundle(AssetDatabase.LoadAssetAtPath("Assets/ui/"+file.Name,typeof(UnityEngine.Object)) as UnityEngine.Object,
 				                               null,
-				                               "Assets/StreamingAssets/"+file.Name.Replace(".prefab",".u3d"),options, BuildTarget.Android);
+				                               "Assets/StreamingAssets/ui/"+file.Name.Replace(".prefab",".u3d"),options, BuildTarget.Android);
 			}
 		}
 	}
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,12 +6,21 @@ public class Main : MonoBehaviour {
 	// Use this for initialization
 	public static Main main;
 	private Transform _transform;
+	//不同平台下StreamingAssets的路径是不同的
+		public static readonly string PathURL =
+#if UNITY_ANDROID   //安卓
+			"jar:file://" + Application.dataPath + "!/assets/";
+#elif UNITY_IPHONE  //iPhone
+			Application.dataPath + "/Raw/";
+#elif UNITY_STANDALONE_WIN || UNITY_EDITOR  //windows平台和web平台
+			"file://" + Application.dataPath + "/StreamingAssets/";
+#else
+			string.Empty;
+#endif
 	void Start () {
 		main = this;
-
 		_transform = gameObject.transform;
 		ShowView("ui/LoginView");
-
 	}
 	void Awake ()
 	{
@@ -22,9 +31,33 @@ public class Main : MonoBehaviour {
 		Destroy(go);
 	}
 	public void ShowView(string path){
-		prefab = Instantiate(Resources.Load(path),_transform.position,_transform.rotation) as GameObject;
-		prefab.transform.parent = _transform;
-		prefab.transform.localScale = _transform.localScale;
+
+		Object go = Resources.Load(path,typeof(GameObject));
+		if(go!=null){
+			InstantiateViews(go);
+		}else{
+#if UNITY_EDITOR
+			InstantiateViews(Resources.LoadAssetAtPath("Assets/"+path+".prefab",typeof(GameObject)));
+#else
+			StartCoroutine(LoadMainGameObject(PathURL+path+".u3d"));
+#endif
+		}
+	}
+	private IEnumerator LoadMainGameObject(string path)
+	{
+		Debug.Log(path);
+		WWW bundle = new WWW(path);
+		yield return bundle;
+
+		yield return InstantiateViews(bundle.assetBundle.mainAsset);
+		bundle.assetBundle.Unload(false);
 	}
 
+	public GameObject InstantiateViews(Object go){
+
+		prefab = Instantiate(go,_transform.position,_transform.rotation) as GameObject;
+		prefab.transform.parent = _transform;
+		prefab.transform.localScale = _transform.localScale;
+		return prefab;
+	}
 }
