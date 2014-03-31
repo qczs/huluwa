6c5a643166e468e6012b68dbccaf27b26360f02e
赵鹤楠 <qczs88@gmail.com>
2014-03-31 14:02:38 +0800

    角色属性管理

---
+++ Assets/Scripts/role/RoleInfo.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoleInfo {
+
+	private long roleId;
+	
+	public long RoleId {
+		get { return roleId; }
+		set { roleId = value; }
+	}
+
+	private string roleName;
+	
+	public string RoleName {
+		get { return roleName; }
+		set { roleName = value; }
+	}
+
+	private string roleImg;
+	
+	public string RoleImg {
+		get { return roleImg; }
+		set { roleImg = value; }
+	}
+
+	private int rolelv;
+	
+	public int Rolelv {
+		get { return rolelv; }
+		set { rolelv = value; }
+	}
+
+	private int viplv;
+	
+	public int Viplv {
+		get { return viplv; }
+		set { viplv = value; }
+	}
+
+	private int exp;
+	
+	public int Exp {
+		get { return exp; }
+		set { exp = value; }
+	}
+
+	private int power;
+	
+	public int Power {
+		get { return power; }
+		set { power = value; }
+	}
+
+	private int gold;
+	
+	public int Gold {
+		get { return gold; }
+		set { gold = value; }
+	}
+
+	private int silver;
+	
+	public int Silver {
+		get { return silver; }
+		set { silver = value; }
+	}
+
+	private int jianghun;
+	
+	public int Jianghun {
+		get { return jianghun; }
+		set { jianghun = value; }
+	}
+
+	private int hunyu;
+	
+	public int Hunyu {
+		get { return hunyu; }
+		set { hunyu = value; }
+	}
+
+	private int physical;
+	
+	public int Physical {
+		get { return physical; }
+		set { physical = value; }
+	}
+
+	private int stamina;
+	
+	public int Stamina {
+		get { return stamina; }
+		set { stamina = value; }
+	}
+
+	private int physicalCount;
+	
+	public int PhysicalCount {
+		get { return physicalCount; }
+		set { physicalCount = value; }
+	}
+	
+	private int staminaCount;
+	
+	public int StaminaCount {
+		get { return staminaCount; }
+		set { staminaCount = value; }
+	}
+
+}
---
+++ Assets/Scripts/role/RoleManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoleManager {
+
+	private static RoleManager instance;
+
+	public static RoleManager getInstance ()
+	{
+		if (instance == null) {
+			instance = new RoleManager ();
+			init();
+
+		}
+		return instance;  
+	}
+
+	private static RoleInfo roleInfo;
+
+	public static void init(){
+		roleInfo = new RoleInfo();
+	}
+
+	public void clear(){
+		roleInfo = null;
+		instance = null;
+	}
+
+	public RoleInfo RoleInfo {
+		get { return roleInfo; }
+		set { roleInfo = value; }
+	}
+	
+
+}
