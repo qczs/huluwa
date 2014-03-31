d3acfaa2f60a27af8a40b6f1fc26331d62957d21
赵鹤楠 <qczs88@gmail.com>
2014-03-31 14:29:23 +0800

    修改bean的方法

--- Assets/Scripts/ResourcesData/Skills.cs
+++ Assets/Scripts/ResourcesData/Skills.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Xml;
 
 public class Skills {
 
@@ -11,7 +12,24 @@ public class Skills {
 	
 	public int equipCondition{ get; set; }
 
+	public static Skills getInstance(string id){
+		Skills skills = new Skills ();
+		XmlElement note = ResourceManager.getInstance().getXmlAttribute ("skills", "skillID", id);
+		skills.skillID = int.Parse (note.GetAttribute ("skillID"));
+		skills.skillType = int.Parse (note.GetAttribute ("skillType"));
+		skills.skillName = note.GetAttribute ("skillName");
+		skills.equipCondition = int.Parse (note.GetAttribute ("equipCondition"));
+		return skills;
+	}
+	
+	public static string getKey(string id){
+		return "skills_skillID_" + id;
+	}
+
 	public override string ToString(){
 		return "skillID:" + skillID+" skillType:"+skillType+" skillName:"+skillName+" equipCondition:"+equipCondition;
 	}
+
+
+
 }
--- Assets/Scripts/utils/ResourceManager.cs
+++ Assets/Scripts/utils/ResourceManager.cs
@@ -5,51 +5,57 @@ using System;
 
 public class ResourceManager
 {
-
-		private static ResourceManager instance;
-
-		public static ResourceManager getInstance ()
-		{
-				if (instance == null) {
-//						if (Debug.isDebugBuild) {
-//								Debug.Log ("aaaaa");
-//						}
-						instance = new ResourceManager ();
-				}
-				return instance;  
+	
+	private static ResourceManager instance;
+	private static Hashtable dataCache;
+	
+	public static ResourceManager getInstance ()
+	{
+		if (instance == null) {
+			instance = new ResourceManager ();
+			dataCache = new Hashtable ();
 		}
-
-		public Skills getSkills(string id)
-		{
-				Skills skills = new Skills();
-				XmlElement note = getXmlAttribute ("skills", "skillID", id);
-				skills.skillID = int.Parse(note.GetAttribute("skillID"));
-				skills.skillType = int.Parse(note.GetAttribute("skillType"));
-				skills.skillName = note.GetAttribute("skillName");
-				skills.equipCondition = int.Parse(note.GetAttribute("equipCondition"));
-
-				return skills;
-		}
-
-		// skills  skillID
-		public XmlElement getXmlAttribute (string xmlName, string idName, string id)
-		{
-//				long a = TimeUtil.UNIX_TIMESTAMP (System.DateTime.Now);
-//				Debug.Log ("a :" + a);
-
-				string url = Application.dataPath + "/Resources/data/" + xmlName + ".xml";
-//				if (Debug.isDebugBuild) {
-//						Debug.Log ("url:" + url);
-//				}
-				XmlDocument xmldoc = new XmlDocument ();
+		return instance;  
+	}
+	
+	public XmlElement getXmlAttribute (string xmlName, string idName, string id)
+	{
+		//				long a = TimeUtil.UNIX_TIMESTAMP (System.DateTime.Now);
+		//				Debug.Log ("a :" + a);
 		
-				xmldoc.Load (url);
-				XmlElement note = (XmlElement)xmldoc.SelectSingleNode ("/datas/data[@" + idName + "='" + id + "']");
-//				if (n != null) {
-//						Debug.Log (n.GetAttribute ("skillID"));
-//				}
-//				Debug.Log ("time :" + (TimeUtil.UNIX_TIMESTAMP (System.DateTime.Now) - a));
-				return note;
+		string url = Application.dataPath + "/Resources/data/" + xmlName + ".xml";
+		//				if (Debug.isDebugBuild) {
+		//						Debug.Log ("url:" + url);
+		//				}
+		XmlDocument xmldoc = new XmlDocument ();
+		
+		xmldoc.Load (url);
+		XmlElement note = (XmlElement)xmldoc.SelectSingleNode ("/datas/data[@" + idName + "='" + id + "']");
+		//				if (n != null) {
+		//						Debug.Log (n.GetAttribute ("skillID"));
+		//				}
+		//				Debug.Log ("time :" + (TimeUtil.UNIX_TIMESTAMP (System.DateTime.Now) - a));
+		return note;
+	}
+	
+	public void clear ()
+	{
+		instance = null;
+		dataCache.Clear ();
+		dataCache = null;
+	}
+	
+	public Skills getSkills (string id)
+	{
+		Skills skills;
+		String key = Skills.getKey (id);
+		if (dataCache.Contains (key) == false) {
+			skills = Skills.getInstance (id);
+			dataCache.Add (key, skills);
+		} else {
+			skills = (Skills)dataCache [key];
 		}
-
+		return skills;
+	}
+	
 }
